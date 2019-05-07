18feeada137fd25e7bbe29330df0a50b3481a74e
黄志标 <1169078896@qq.com>
2019-05-07 16:40:59 +0800

    处理数据导入

--- app/Jobs/ImportDataJob.php
+++ app/Jobs/ImportDataJob.php
@@ -15,15 +15,16 @@ class ImportDataJob implements ShouldQueue
 
     protected $file_path;
     protected $repo;
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(string $file_path,$repo)
+    public function __construct(string $file_path, $repo)
     {
-        $this->file_path=$file_path;
-        $this->repo=$repo;
+        $this->file_path = $file_path;
+        $this->repo = $repo;
     }
 
     /**
@@ -33,14 +34,17 @@ class ImportDataJob implements ShouldQueue
      */
     public function handle()
     {
-        //获取当前文本编码格式
-        $content = file_get_contents($this->file_path);
-        $fileType = mb_detect_encoding($content, array('UTF-8', 'GBK', 'LATIN1', 'BIG5'));
-
-        app(Excel::class)->load($this->file_path, function ($reader) {
-            $rows = $reader->all();
-            // 处理导入的数据
-            $this->repo->handleUploadData($rows);
-        }, $fileType);//以指定的编码格式打开文件
+        try {//获取当前文本编码格式
+            $content = file_get_contents($this->file_path);
+            $fileType = mb_detect_encoding($content, array('UTF-8', 'GBK', 'LATIN1', 'BIG5'));
+            app(Excel::class)->load($this->file_path, function ($reader) {
+                $rows = $reader->all();
+                // 处理导入的数据
+                $this->repo->handleUploadData($rows);
+            }, $fileType);//以指定的编码格式打开文件
+        } catch (\Exception $e) {
+            \Log::error('数据导入失败，file_path:' . $this->file_path);
+            \Log::error('数据导入失败，error:' . $e->getMessage());
+        }
     }
 }
