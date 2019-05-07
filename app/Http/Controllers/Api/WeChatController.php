abd1c63bdc97bb077b6dc80875f9de67747738ee
RockyHuas <1169078896@qq.com>
2019-05-07 21:35:02 +0800

    no

--- app/Http/Controllers/Api/WeChatController.php
+++ app/Http/Controllers/Api/WeChatController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiRequest;
 
 class WeChatController extends Controller
 {
