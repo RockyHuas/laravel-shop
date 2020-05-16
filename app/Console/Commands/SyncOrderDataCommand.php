158cead2f3aab9eef1b7f87f79acba23d2b65409
RockyHuas <1169078896@qq.com>
2020-05-16 16:10:11 +0800

    同步订单数据

---
+++ app/Console/Commands/SyncOrderDataCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\OrderShipped;
+use App\Models\Order;
+
+class SyncOrderDataCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'order:sync';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '订单数据同步';
+
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->info('开始同步订单数据');
+        Order::with('order_items.product')->chunk(2000,function($orders){
+            $orders->each(function($order){
+                $order->order_items->each(function($order_item){
+                    $product=$order_item->product;
+                    $order_item->update([
+                        'protuct_title'=>data_get($product,'title'),
+                        'product_sku_id'=>0
+                    ]);
+                });
+            });
+        });
+    }
+}
